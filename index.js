92a825e9be0bd8685ab1d4c1e1d30f6530b41d57
alasalle <angela.lasalle91@gmail.com>
2020-01-07 23:16:15 -0500

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from "express";const app = express();
+const PORT = 4300;app.get("/", (req, res) => {
+  res.json({
+    message: "Notetaking API v1"
+  });
+});
+app.listen(PORT, () => {
+  console.log(`Server is listening on PORT ${PORT}`);
+});
